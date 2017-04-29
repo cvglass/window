0a62e159a6bfa49e7eb298b517cc5e914ff0cfc8
cvglass <christinevglass@gmail.com>
2017-04-29 14:52:52 -0500

    connected react routes to server

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Home = () => {
+  return (
+    <h3>HOME</h3>
+  )
+}
+
+export default Home
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+//components
+import Home from './components/home'
+
+const routes = (
+  <Route path="/" component={Home}>
+  </Route>
+);
+
+export default routes
+
