2c3c4ecb093bedc1107a45c996f6541d40f58856
cvglass <christinevglass@gmail.com>
2017-05-01 19:33:08 -0500

    webpack config works

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,29 @@
-const React = require('react');
-// const reactRouter = require('react-router');
-const Route = require('react-router').Route;
+// const React = require('react');
+// // const reactRouter = require('react-router');
+// const Route = require('react-router').Route;
 
-//components
-const Home = require('./components/home');
+// //components
+// const Home = require('./components/home');
 
-class routes extends React.Component {
-  render() {
-    return(
-      <Route path="/" component={Home} />
-    )
-  }
-}
+// class routes extends React.Component {
+//   render() {
+//     return(
+//       <Route path="/" component={Home} />
+//     )
+//   }
+// }
+
+import React from 'react'
+import { Route, IndexRoute } from 'react-router'
+import Home from './components/home'
+// import Layout from './components/Layout';
+// import IndexPage from './components/IndexPage';
+// import AthletePage from './components/AthletePage';
+// import NotFoundPage from './components/NotFoundPage';
+
+const routes = (
+  <Route path="/" component={Home}>
+  </Route>
+);
+
+export default routes;
--- src/server.js
+++ src/server.js
@@ -1,42 +1,60 @@
-'use strict'
+import path from 'path';
+import { Server } from 'http';
+import Express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import routes from './routes';
+// import NotFoundPage from './components/NotFoundPage';
 
-const path = require('path');
-const Server = require('http').Server;
-const express = require('express');
-const React = require('react');
-const routes = require('./routes');
-const renderToString = require('react-dom/server');
-const reactRouter = require('react-router');
-const match = reactRouter.match;
-const RouterContext = reactRouter.RouterContext
-
-const app = new express();
+// initialize the server and configure support for ejs templates
+const app = new Express();
 const server = new Server(app);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'static')));
+// define the folder that will be used for static assets
+app.use(Express.static(path.join(__dirname, 'static')));
 
+// universal routing and rendering
 app.get('*', (req, res) => {
   match(
-    {routes: routes, location: req.url},
-    (err, redirectionLocation, renderProps) => {
+    { routes, location: req.url },
+    (err, redirectLocation, renderProps) => {
+
+      // in case of error display the error message
       if (err) {
-        return res.status(500).send(err.message)
+        return res.status(500).send(err.message);
       }
 
-      if (redirectionLocation) {
-        return res.redirect(302, redirectionLocation.pathname + redirectionLocation.search)
+      // in case of redirect propagate the redirect to the browser
+      if (redirectLocation) {
+        return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
       }
 
-      return res.render('index')
+      // generate the React markup for the current route
+      let markup;
+      if (renderProps) {
+        // if the current route matched we have renderProps
+        markup = renderToString(<RouterContext {...renderProps}/>);
+      } else {
+        // otherwise we can render a 404 page
+        markup = renderToString(<NotFoundPage/>);
+        res.status(404);
+      }
+
+      // render the index template with the embedded React markup
+      return res.render('index', { markup });
     }
-  )
-})
+  );
+});
 
+// start the server
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'production';
 server.listen(port, err => {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
   console.info(`Server running on http://localhost:${port} [${env}]`);
-})
+});
